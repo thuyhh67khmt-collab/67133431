eae6b3f682052923ab710b0f666f7af10adc656d
thuyhh67khmt-collab <thuy.hh.67khmt@ntu.edu.vn>
2025-12-08 14:08:24 +0700

    Nop bai
    Nop bai ngay 8 thang 12

---
+++ Tinh tong 3 so abc.py
@@ -0,0 +1,5 @@
+a=int( input ( "Nhap so a "))
+b=int( input ( "Nhap so b "))
+c=int( input ( "Nhap so c "))
+         
+print( "Tong=", a+b+c )        
---
+++ Viet chuong trinh nhap vao mot so nguyen cho biet so do co chia het cho dong thoi 3 va 5 hay khong.py
@@ -0,0 +1,6 @@
+n=int(input("Nhập số nguyên: "))
+
+if n % 3 == 0 and n % 5 ==0:
+   print(f"{n} chia het dong thoi cho 3 va 5.")
+else:
+   print(f"{n} Khong chia het dong thoi cho 3 va 5.")
