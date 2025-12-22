2e3841120ed65957b8787f6637820f3dffcf8284
thuyhh67khmt-collab <thuy.hh.67khmt@ntu.edu.vn>
2025-12-22 14:54:10 +0700

    Create so lon nhat,so nho nhat.py

---
+++ so lon nhat,so nho nhat.py
@@ -0,0 +1,22 @@
+a=int(input(" Nhap so a:"))
+b=int(input(" Nhap so b:"))
+c=int(input(" Nhap so c:"))
+max=a
+min=
+if b>max:
+    max=b
+if c>max:
+    max=c
+if b<min:
+    min=b
+if c<min:
+    min=c
+print('so lon nhat la:', max)
+print('so nho nhat la:',min)
+
+    
+    
+    
+
+
+
