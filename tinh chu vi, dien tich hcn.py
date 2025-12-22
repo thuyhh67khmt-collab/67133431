c41ef704f9a5c5f6f72717f2512f424a66f7a65f
thuyhh67khmt-collab <thuy.hh.67khmt@ntu.edu.vn>
2025-12-22 14:30:48 +0700

    Create tinh chu vi, dien tich hcn.py

---
+++ tinh chu vi, dien tich hcn.py
@@ -0,0 +1,12 @@
+
+while True:
+    h=float (input(" Nhap chieu rong:"))
+    w=float (input(" Nhap chieu dai:"))
+    if 0.0<=w and h<=100.0:
+                  cv=(w+h)*2
+                  dt=(w*h)
+    break
+print( f" chu vi hcn:{cv:9.1f}")
+print( f" dien tich hcn:{dt:9.1f}")
+
+    
